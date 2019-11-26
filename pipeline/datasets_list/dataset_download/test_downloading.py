0e051bf7fe55c42f3d978c29370d343119521d64
Chaosli98 <56908622+Chaosli98@users.noreply.github.com>
2019-11-26 12:54:04 +0800

    Add files via upload

--- pipeline/datasets_list/dataset_download/test_downloading.py
+++ pipeline/datasets_list/dataset_download/test_downloading.py
@@ -1,6 +1,6 @@
 import importlib.util
 import sys
-downloader_path = '/home/ztr/abio_database_pipeline/downloader.py'
+downloader_path = '/home/ztr/abio_database_pipeline/pipeline/datasets_list/dataset_download/downloader.py'
 downloader_spec = importlib.util.spec_from_file_location('downloader', downloader_path)
 downloader = importlib.util.module_from_spec(downloader_spec)
 downloader_spec.loader.exec_module(downloader)
